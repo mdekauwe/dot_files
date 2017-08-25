32e23ec10b81a2d98a0cf72dd732e185fa6ce8df
Martin De Kauwe <mdekauwe@gmail.com>
2017-08-25 21:39:05 +1000

    Updated atom stuff

--- install_files.sh
+++ install_files.sh
@@ -7,3 +7,15 @@ cp $FDIR/bash_profile ~/.bash_profile
 FDIR="spell"
 cp $FDIR/aspell.en.prepl ~/.aspell.en.prepl
 cp $FDIR/aspell.en.pws ~/.aspell.en.pws
+
+FDIR="atom"
+cp $FDIR/config.cson ~/.atom/
+cp $FDIR/github.cson ~/.atom/
+cp $FDIR/kite-config.json ~/.atom/
+cp $FDIR/snippets.cson ~/.atom/
+cp $FDIR/init.coffee ~/.atom/
+cp $FDIR/keymap.cson ~/.atom/
+cp $FDIR/packages.list ~/.atom/
+cp $FDIR/styles.less ~/.atom/
+
+apm install --packages-file $FDIR/packages.list
--- install_ports.sh
+++ install_ports.sh
@@ -7,3 +7,5 @@ sudo port select --set python python36
 
 sudo port install aspell
 sudo port install aspell-dict-en
+
+sudo port install R
