c9ce4e2689e0d86e655ef95080767833febdefbd
Martin De Kauwe <mdekauwe@gmail.com>
2017-08-25 21:15:28 +1000

    Added aspell files

--- get_files.sh
+++ get_files.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 FDIR="bash"
-
 cp ~/.bashrc $FDIR/bashrc
 cp ~/.bash_profile $FDIR/bash_profile
+
+FDIR="spell"
+cp ~/.aspell.en.prepl $FDIR/aspell.en.prepl
+cp ~/.aspell.en.pws $FDIR/aspell.en.pws
--- install_files.sh
+++ install_files.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 FDIR="bash"
-
 cp $FDIR/bashrc ~/.bashrc
 cp $FDIR/bash_profile ~/.bash_profile
+
+FDIR="spell"
+cp $FDIR/aspell.en.prepl ~/.aspell.en.prepl
+cp $FDIR/aspell.en.pws ~/.aspell.en.pws
